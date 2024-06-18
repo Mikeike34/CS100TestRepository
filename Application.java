fbd79f11e68de62c198006a9dacc0dcd34b11dc8
Michael Cantone <michaeljcantone@gmail.com>
2024-06-17 17:48:22 -0700

    Updated Application.java with infrastructure code

---
+++ Application.java
@@ -0,0 +1,12 @@
+
+public class Application{
+	
+	public static void main(String args[]){
+		
+		System.out.println("My Application \n=================");
+
+		System.out.println("\n\n\n\n\n\n\n\n");
+		
+		System.out.println("=================");
+	}
+}
